4fcaa5fa4a0224ae176d81839306375cfa1c5d7a
Tomas Colazo <405477@tecnicatura.frc.edu.ar>
2024-09-08 15:21:48 -0300

    config server

---
+++ config-service/src/test/java/tecnicatura/frc/utn/edu/ar/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tecnicatura.frc.utn.edu.ar.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
