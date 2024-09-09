1c01ff34c9632dd072cb2d40ae801f32184c7a3a
Tomas Colazo <405477@tecnicatura.frc.edu.ar>
2024-09-09 11:09:48 -0300

    eureka

---
+++ eureka-service/src/test/java/tecnicatura/frc/utn/edu/ar/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tecnicatura.frc.utn.edu.ar.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
